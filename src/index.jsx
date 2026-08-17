6faeefaf9234baa9090abdb10945de695c40f2ea
JessicaD3 <jessica.dubois1003@gmail.com>
2026-08-17 03:25:03 +0200

    Connexion Notification aux CRUD

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './App.css';
 import { ThemeProvider } from './contexts/ThemeContext';
+import { NotificationProvider } from './contexts/NotificationContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <NotificationProvider>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </NotificationProvider>
   </React.StrictMode>
 );
